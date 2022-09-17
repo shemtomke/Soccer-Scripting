a568764df13d141c41d5774d0d842110e4822ec5
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2022-09-17 18:15:08 +0300

    spawn

--- Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -15,12 +15,17 @@ public class SpawnManagerX : MonoBehaviour
     public int waveCount = 1;
 
 
-    public GameObject player; 
+    public GameObject player;
+
+    private void Start()
+    {
+        SpawnEnemyWave(waveCount);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        enemyCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
 
         if (enemyCount == 0)
         {
@@ -49,7 +54,7 @@ public class SpawnManagerX : MonoBehaviour
         }
 
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
